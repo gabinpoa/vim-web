d5a57cc195140023745cbc6c2f76573239788cd6
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-01-14 18:49:19 +0000

    Add Svelte initial project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let files: FileList;
+	function handleFileOpen() {
+		const selectecFile = files.length === 1 ? files[1] : null;
+
+		if (selectecFile) {
+			const reader = new FileReader();
+			reader.onload = () => {
+				reader.readAsArrayBuffer(selectecFile);
+			};
+		}
+	}
+</script>
+
+<form>
+	<label for="file">Open a text based file</label>
+	<input on:change={handleFileOpen} name="value" bind:files type="file" />
+</form>
